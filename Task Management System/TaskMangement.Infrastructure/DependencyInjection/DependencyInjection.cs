8d5eca950ea55a0bf98940b3536cabcf1a86e1e7
AbinayaSelvamS <AbinayaSelvamS-github-ssas86831@gmail.com>
2025-09-07 13:39:10 +0530

    Add initial Task Management System project files

---
+++ Task Management System/TaskMangement.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Data.Context;
+using TaskManagement.Data.IRepositories;
+using TaskManagement.Data.Repositories;
+using TaskMangement.Infrastructure.Mapping;
+
+namespace TaskMangement.Infrastructure.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddInfrastructure(
+         this IServiceCollection services,
+         IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IProjectRepository, ProjectRepository>();
+           
+            services.AddAutoMapper(typeof(MappingProfile));
+
+            return services;
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Task Management System/TaskMangement.Infrastructure/Mapping/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using TaskManagement.Common.DTOs.RequestDTO;
+using TaskManagement.Common.DTOs.ResponseDTO;
+using TaskManagement.Data.Entities;
+
+
+namespace TaskMangement.Infrastructure.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Project, AddProjectResponseDTO>();
+            CreateMap<AddProjectRequestDto, Project>();
+        }   
+    }
+}
